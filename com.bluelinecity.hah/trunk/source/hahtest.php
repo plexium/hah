f966cb024da2c6ff2271e26c3b7f27e1c48d6bc7
benglish <benglish@e26cb320-6b0a-0410-892d-e6db4bb496c4>
2012-05-31 15:15:58 +0000

    - merged trunk hah with tag version i been fixing in plexframe - removed table tag stuff and made it super simple - some bug fixes made when working with plexframe

--- com.bluelinecity.hah/trunk/source/hahtest.php
+++ com.bluelinecity.hah/trunk/source/hahtest.php
@@ -15,6 +15,7 @@ $test->set('outvar', 5);
 $test->set('colorlist', array('red','blue','green','black','white'));
 $test->set('hash', array('one' => 1, 'two' => 2, 'three' => 3));
 $test->set('contacts', array(
+   array('id' => 'id', 'name' => 'name', 'phone' => 'phone', 'age' => 'age', 'dob' => 'dob'),
    array('id' => 1, 'name' => 'Bob Smith', 'phone' => '810-555-5555', 'age' => 30, 'dob' => '1979-07-17'),
    array('id' => 2, 'name' => 'Rob Smith', 'phone' => null, 'age' => 12, 'dob' => '1999-07-17'),
    array('id' => 3, 'name' => 'Larry Smith', 'phone' => '810-555-1234', 'age' => 66, 'dob' => '1939-07-17'),
